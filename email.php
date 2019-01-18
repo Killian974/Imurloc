61c3b005fe60dd3110134feecdfed1e1a1c96bde
Killian974 <bellour.killian@gmail.com>
2019-01-18 09:49:23 +0100

    update

--- email.php
+++ email.php
@@ -3,15 +3,17 @@ try
 {
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
     $bdd = new PDO('mysql:host=localhost;dbname=imurloc','root','', $pdo_options);
-    if(isset($_POST['cp1a']))
+    if(isset($_POST['email']))
     {
         
         $mail = $_POST['email'];
         $bdd->exec("INSERT INTO utilisateur (mail) VALUES ('$mail')");
+        echo("Ok");
 }
 }
 catch (Exception $e)
 {
-        die('Erreur : ' . $e->getMessage());
+        // die('Erreur : ' . $e->getMessage());
+        echo("No");
 }
 ?>
